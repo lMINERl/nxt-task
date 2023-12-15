ac676656118629e8916593a952a8025185baf64c
lMINERl <seect19944@gmail.com>
2023-12-15 15:22:10 +0200

    inital setup

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppDrawer from "../components/AppDrawer";
+
+const App = () => {
+  return (
+    <div>
+      <AppDrawer />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
